dff511c96ab3db3072e7f92b46a618b75fb82418
JOYAL RAJU <51167856+joyalraju@users.noreply.github.com>
2019-08-05 11:40:59 -0400

    file opening using fstream

---
+++ coding/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<fstream>
+using namespace std;
+int main() {
+	ifstream a;
+	a.open("words.txt");
+if(a.is_open())
+	{cout<<"file found";
+	a.close();
+	}
+else
+				cout<<"File not found \n";
+
+return 0;
+}
+
